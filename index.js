99a55bd26c0aeb983e338bf02fbecd7c24ce1c2b
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-12-30 23:52:05 -0800

    adding cors restrictions

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //use cors
-app.use(cors());
+app.use(cors({
+    origin: "https://videogamelist.netlify.app/",
+}));
 
 app.use(routes);
 
